ca9ca398b88d0dd912ad9594e636cd91d4edad79
Anderson Lopes de Oliveira <>
2021-04-29 21:36:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    
+    int A, B, SOMA;
+    Scanner scan = new Scanner(System.in);
+
+    A = scan.nextInt();
+    B = scan.nextInt();
+
+    SOMA = A + B;
+
+        
+    System.out.println("SOMA = " + SOMA);
+  }
+}
